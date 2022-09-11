5bf5abdb64735e0637dcfd9949287d3ed59a500f
omar ezzdeen <omar.izziddeen@gmail.com>
2022-09-11 17:47:21 +0300

    create charts

--- app/src/main/java/com/karrar/betterlife/ui/MainActivity.kt
+++ app/src/main/java/com/karrar/betterlife/ui/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // just to test
         val aaCharts = Charts(
             dataOfHabits = arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2),
             dataOfPlans = arrayOf(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0))
