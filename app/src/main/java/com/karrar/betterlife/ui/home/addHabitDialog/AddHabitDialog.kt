06ef2e35e7846e5f41efd2241ccac8d10b0504bd
amnah.a <amnah44ali@gmail.com>
2022-09-13 19:44:58 +0300

    add habit to database and show it in home

--- app/src/main/java/com/karrar/betterlife/ui/home/addHabitDialog/AddHabitDialog.kt
+++ app/src/main/java/com/karrar/betterlife/ui/home/addHabitDialog/AddHabitDialog.kt
@@ -1,5 +1,9 @@
 package com.karrar.betterlife.ui.home.addHabitDialog
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.karrar.betterlife.R
 import com.karrar.betterlife.databinding.DialogAddHabitBinding
 import com.karrar.betterlife.ui.base.BaseDialogFragment
@@ -10,7 +14,14 @@ class AddHabitDialog : BaseDialogFragment<DialogAddHabitBinding, AddHabitViewMod
     override val layoutIdFragment: Int = R.layout.dialog_add_habit
 
     override fun setup() {
+        addNewHabitAndBackToHome()
+    }
 
+    private fun addNewHabitAndBackToHome(){
+        viewModel.isAddHabit.observe(viewLifecycleOwner){
+            findNavController().popBackStack()
+            Toast.makeText(this.context, "Add habit is successfully", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
--- app/src/main/java/com/karrar/betterlife/ui/home/addHabitDialog/AddHabitViewModel.kt
+++ app/src/main/java/com/karrar/betterlife/ui/home/addHabitDialog/AddHabitViewModel.kt
@@ -1,8 +1,30 @@
 package com.karrar.betterlife.ui.home.addHabitDialog
 
 import androidx.lifecycle.*
+import com.karrar.betterlife.data.database.entity.Habit
+import com.karrar.betterlife.data.repository.BetterRepository
+import kotlinx.coroutines.launch
 
 
 class AddHabitViewModel : ViewModel() {
+    private val repository = BetterRepository()
 
+    val habitName = MutableLiveData<String?>()
+
+    private val _isAddHabit = MutableLiveData<Boolean>()
+    val isAddHabit: LiveData<Boolean>
+        get() = _isAddHabit
+
+
+    fun addNewHabit() {
+        _isAddHabit.postValue(true)
+        viewModelScope.launch {
+            repository.insertNewHabit(
+                Habit(
+                    name = habitName.value.toString(),
+                    point = 0
+                )
+            )
+        }
+    }
 }
