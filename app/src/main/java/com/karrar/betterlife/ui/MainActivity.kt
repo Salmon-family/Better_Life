0d9cc0d098a4db91c6f41044e92502bb51af2475
AmeerAmjed <pr.ameer98@gmail.com>
2022-09-20 00:12:04 +0300

    add bottom navigation bar

--- app/src/main/java/com/karrar/betterlife/ui/MainActivity.kt
+++ app/src/main/java/com/karrar/betterlife/ui/MainActivity.kt
@@ -3,15 +3,35 @@ package com.karrar.betterlife.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.karrar.betterlife.R
+import com.karrar.betterlife.databinding.ActivityMainBinding
 import com.karrar.betterlife.util.workManager.NotificationScheduler
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         NotificationScheduler(this).initialDelay()
+
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigationBar.setupWithNavController(navController)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupNavigation()
     }
 }
